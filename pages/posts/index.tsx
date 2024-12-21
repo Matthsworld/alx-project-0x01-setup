0e96b77cf87305140b9c9861e3399733ad901754
Matthsworld <Matthsworld@gmail.com>
2024-12-21 21:59:02 +0100

    add:  Set up navigation between pages

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
-    return (
-      <div className="p-4">
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
         <h1 className="text-2xl font-bold">Posts</h1>
-      </div>
-    );
-  };
-  
-  export default Posts;
-  
+      </main>
+    </div>
+  );
+};
+
+export default Posts;
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const Users: React.FC = () => {
-    return (
-      <div className="p-4">
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
         <h1 className="text-2xl font-bold">Users</h1>
-      </div>
-    );
-  };
-  
-  export default Users;
-  
+      </main>
+    </div>
+  );
+};
+
+export default Users;
